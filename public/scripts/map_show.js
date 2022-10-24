32ce015b1fe3bb09e9da414e9fdc5b6be08e2817
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-10-24 18:26:41 +0000

    Finalize map-rendering code

--- public/scripts/map_show.js
+++ public/scripts/map_show.js
@@ -5,7 +5,7 @@ $(() => {
     method: 'GET',
     url: `/api/maps/${id}`
   })
-  .done((response) => {
+  .then((response) => {
     const map = response.map;
     const $mapDetails = $('#map-details');
     $mapDetails.empty();
@@ -13,8 +13,17 @@ $(() => {
     $(`<p class="name">`).text('By: ' + map.name).appendTo($mapDetails);
     $(`<p class="created_date">`).text('Created: ' + map.created_date).appendTo($mapDetails);
     $(`<p class="description">`).text('Description: ' + map.description).appendTo($mapDetails);
-    // google maps code
+    // Google Maps render code
+    let gMap = google.maps.Map;
+    const mapOptions = {
+      zoom: map.zoom,
+      center: { lat: map.lat, lng: map.lng },
+    };
+    gMap = new google.maps.Map(
+      document.getElementById('google-map'),
+      mapOptions,
+    );
   });
 });
 
-// code here
+
