515c6119d150c8285e9472afc374377daf704f2e
Kevin Lee <jhssttj@hotmail.com>
2022-10-26 16:44:40 -0600

    Finalize CSS styling for list of maps page and minor change to create/update maps page

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -8,7 +8,7 @@ $(() => {
     const $mapsList = $('#maps');
     $mapsList.empty();
     for(const map of response.maps) {
-      $(`<li><a href="/maps/${map.id}" class="map">${map.title}</a> by <i>${map.name}</i> — "${map.description}"</li>`).appendTo($mapsList);
+      $(`<li id = "map_info"><a href="/maps/${map.id}" class="map">${map.title}</a> <i> by ${map.name} <br> "${map.description}"</li>`).appendTo($mapsList);
     }
   });
 });
