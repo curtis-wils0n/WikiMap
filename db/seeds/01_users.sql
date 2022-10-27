6515a73ac6f66a0abfa2ad7a036825f153f2058d
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-10-27 21:44:56 +0000

    Rework database to only include Canadian examples, change user's names

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
+INSERT INTO users (name, username, password, email, profile_picture_url) VALUES ('Kevin', 'kevin', 'password', 'kevin@kevin.com', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
+INSERT INTO users (name, username, password, email, profile_picture_url) VALUES ('Curtis', 'curtis', 'password', 'curtis@curtis.com', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
 INSERT INTO users (name, username, password, email, profile_picture_url) VALUES ('Alice', 'alice', 'password', 'alice@alice.com', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
 INSERT INTO users (name, username, password, email, profile_picture_url) VALUES ('Kira', 'kira', 'password', 'kira@kira.com', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
-INSERT INTO users (name, username, password, email, profile_picture_url) VALUES ('Bob', 'bob', 'password', 'bob@bob.com', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
-INSERT INTO users (name, username, password, email, profile_picture_url) VALUES ('Joe', 'joe', 'password', 'joe@joe.com', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,5 +1,5 @@
-INSERT INTO maps (owner_id, title, description, created_date, zoom, lat, lng) VALUES (1, 'The best coffee shops in Calgary', 'Alice''s top 4 coffee spots :D', '2022-02-04', 10, 51.0447, -114.0719);
-INSERT INTO maps (owner_id, title, description, created_date, zoom, lat, lng) VALUES (2, 'Downtown Vancouver murals', 'Murals in and around downtown Vancouver, mostly from 2017ish', '2022-02-04', 10, 49.2827, -123.1207);
-INSERT INTO maps (owner_id, title, description, created_date, zoom, lat, lng) VALUES (3, 'Date Ideas - Toronto', 'Restaurants where I want to take my partner on a date next week', '2022-02-04', 10, 43.6532, -79.3832);
-INSERT INTO maps (owner_id, title, description, created_date, zoom, lat, lng) VALUES (4, 'Bob''s bike route bucket list', 'Places I have always dreamed of biking while I was in Seattle.', '2022-02-04', 10, 47.6062, -122.3321);
+INSERT INTO maps (owner_id, title, description, created_date, zoom, lat, lng) VALUES (1, 'The best coffee shops in Calgary', 'Kevin''s top 4 coffee spots', '2022-02-04', 12, 51.0447, -114.0719);
+INSERT INTO maps (owner_id, title, description, created_date, zoom, lat, lng) VALUES (2, 'Downtown Vancouver murals', 'Murals in and around downtown Vancouver, mostly from 2017ish', '2022-02-04', 12, 49.2827, -123.1207);
+INSERT INTO maps (owner_id, title, description, created_date, zoom, lat, lng) VALUES (3, 'Date Ideas - Toronto', 'Restaurants where I want to take my partner on a date next week', '2022-02-04', 12, 43.6532, -79.3832);
+INSERT INTO maps (owner_id, title, description, created_date, zoom, lat, lng) VALUES (4, 'Kira''s Banff bike route bucket list', 'Places I want to go biking when I''m in Banff.', '2022-02-04', 10, 51.18227906881174, -115.58059641540171);
 
