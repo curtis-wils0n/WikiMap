c54783493cf281c9931e6e01e9fd25266455428c
Kevin Lee <jhssttj@hotmail.com>
2022-10-27 13:47:33 -0600

    fix marker update bug

--- public/scripts/location_show.js
+++ public/scripts/location_show.js
@@ -2,7 +2,7 @@
 $(() => {
   const map_id = $('#map-identifier').attr('value');
   const location_id = $('#location-identifier').attr('value');
-  const user_id = $('#new-location').attr('value');
+  const user_id = Number ($('#new-location').attr('value'));
   $.ajax({
     method: 'GET',
     url: `/api/maps/${map_id}/locations/${location_id}`
@@ -12,6 +12,7 @@ $(() => {
     const editForm = $(`#new-location`);
     const editToggle = $(`#toggleUpdateForm`);
     editToggle.hide();
+
     //Render main info and details accessible to anyone
     $('#title').html(`${location.title}`);
     $('#description').html(`${location.description}`);
@@ -19,7 +20,7 @@ $(() => {
     $('#image').attr("alt", `${location.description}`);
     //Render update features if viewer is creator of location
     const renderEdit = `
-      <form id = 'newLocationForm' action = '/maps/<%= map_id %>/locations/<%= location_id%>' method = 'POST'>
+      <form id = 'newLocationForm' action = '/maps/${map_id}/locations/${location_id}' method = 'POST'>
         <label for="title">Location name:</label>
         <input type="text" name="title" placeholder="Location name" required/>
         <br>
@@ -34,7 +35,7 @@ $(() => {
         <form id = 'deleteForm' method="POST" action="/maps/${map_id}/locations/${location.id}/delete">
         <button type="submit" class="btn btn-primary" id="delete-button">Delete</button>
         </form>`;
-    if( location.creator_id == user_id || location.owner_id == user_id) {
+    if ( location.creator_id === user_id || location.owner_id === user_id) {
       editForm.append(renderEdit);
       editToggle.show();
     };
